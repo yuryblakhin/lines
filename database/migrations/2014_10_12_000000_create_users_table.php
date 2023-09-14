fdc7000bd4e56c38520e8852df455b624e1fb7e7
Yury Blakhin <yuryblakhin@proton.me>
2023-09-14 17:39:40 +0300

    fixed users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('login');
             $table->string('email')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
