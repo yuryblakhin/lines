2edad6fd270992a590893a409372c83151ddca7f
Yury Blakhin <yuryblakhin@proton.me>
2023-09-18 08:27:49 +0300

    added category model

---
+++ app/Models/Category.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Kalnoy\Nestedset\NodeTrait;
+
+class Category extends Model
+{
+    use HasFactory, NodeTrait;
+
+    protected $fillable = [
+        'name',
+        'code',
+        'description',
+        'parent_id',
+    ];
+
+    public function getLftName(): string
+    {
+        return 'lft';
+    }
+
+    public function getRgtName(): string
+    {
+        return 'rgt';
+    }
+}
